ae0651bfed7f9d1441940fd5dd7853242aba689e
Jetsam1 <Akash9105@hotmail.com>
2020-08-04 22:01:36 +0100

    Revert "Revert "1""
    This reverts commit cc7f55cf0fa435d69a546e1f9ee44dadaf8310fd.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,13 +7,16 @@ public class PlayerMovement : MonoBehaviour {
 	private float playerSpeed;
     [SerializeField]
 	private float jumpHeight;
+    [SerializeField]
+    private LayerMask platformsLayerMask;
 
-	////Items to check the player has the ability to jump
-	//public Transform groundCheck;
-	//public float radiusToGround;
-	//public LayerMask groundItems;
-	//private bool isGrounded;
 
+    ////Items to check the player has the ability to jump
+    //public Transform groundCheck;
+    //public float radiusToGround;
+    //public LayerMask groundItems;
+    //private bool isGrounded;
+    private bool doubleJump=true;
 	////Variable to hold double jump
 	//private bool isDoubleJump;
 
@@ -26,6 +29,7 @@ public class PlayerMovement : MonoBehaviour {
         //initilaise player variables
         playerSpeed = 5;
         jumpHeight = 5;
+        //bc2d = transform.GetComponent<BoxCollider2D>();
 
         //get direction the player is moving in
         playerVelocity = GetComponent<Rigidbody2D> ().velocity;
@@ -44,18 +48,35 @@ public class PlayerMovement : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
-		////Check if grounded so double jump is reset
-		//if (isGrounded) {
-		//	isDoubleJump = false;
-		//}
+        ////Check if grounded so double jump is reset
+        //if (isGrounded) {
+        //	isDoubleJump = false;
+        //}
+        if (isGrounded())
+        {
+            doubleJump = true;
+        }
 
 
-		//Get the position of the player at the first step
-		playerVelocity = GetComponent<Rigidbody2D>().velocity;
+        //Get the position of the player at the first step
+        playerVelocity = GetComponent<Rigidbody2D>().velocity;
 
 		//Get the jump axis and have the character jump - using get button instead of axis as we need specific key down actions
 		if (Input.GetButtonDown("Jump")){
-			CharacterJump ();
+			if(isGrounded())
+            {
+                CharacterJump();
+            }
+            else
+            {
+                if(doubleJump)
+                {
+                    CharacterJump();
+                    doubleJump = false;
+                }
+            }
+
+
 
 		}
 
@@ -66,12 +87,37 @@ public class PlayerMovement : MonoBehaviour {
 		//}
 
 		//Use the horizontal axis to have the character move
-		if (Input.GetAxis("Horizontal")!=0){
-			GetComponent<Rigidbody2D>().velocity = new Vector2(playerSpeed*Input.GetAxis("Horizontal"),playerVelocity.y);
-		}
+	//	if (Input.GetAxis("Horizontal")!=0){
+	//		GetComponent<Rigidbody2D>().velocity = new Vector2(playerSpeed*Input.GetAxis("Horizontal"),playerVelocity.y);
+	//	}
+        if(Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(-playerSpeed, GetComponent<Rigidbody2D>().velocity.y);
+        }
+        else
+        {
+           if(Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
+          {
+               GetComponent<Rigidbody2D>().velocity = new Vector2(+playerSpeed, GetComponent<Rigidbody2D>().velocity.y);
+            }
+            else
+            {
+                GetComponent<Rigidbody2D>().velocity = new Vector2(0, GetComponent<Rigidbody2D>().velocity.y);
+            }
+       }
 	}
 
 	private void CharacterJump(){
-		GetComponent<Rigidbody2D>().velocity = new Vector2(playerVelocity.x,jumpHeight);
+        GetComponent<Rigidbody2D>().velocity = Vector2.up * jumpHeight;
+       
 	}
+
+   private bool isGrounded()
+   {
+        RaycastHit2D raycast2d =Physics2D.BoxCast(GetComponent<BoxCollider2D>().bounds.center, GetComponent<BoxCollider2D>().bounds.size, 0f, Vector2.down,.1f,platformsLayerMask); // hamndles ground detection for jumping
+        Debug.Log(raycast2d.collider);
+        return raycast2d.collider != null;
+
+   }
+    
 }
