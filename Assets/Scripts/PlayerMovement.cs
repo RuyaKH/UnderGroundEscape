08fc534ada77dbea9e2ad2e539d62bee13746bbe
Ruya Kumru-Holroyd <ruya.kh@outlook.com>
2020-08-05 03:25:26 +0100

    changes

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,21 +7,23 @@ public class PlayerMovement : MonoBehaviour {
 	private float playerSpeed;
     [SerializeField]
 	private float jumpHeight;
-    [SerializeField]
-    private LayerMask platformsLayerMask;
 
+	////Items to check the player has the ability to jump
+	public Transform groundCheck;
+	public float radiusToGround;
+	public LayerMask groundItems;
+	private bool isGrounded;
 
-    ////Items to check the player has the ability to jump
-    //public Transform groundCheck;
-    //public float radiusToGround;
-    //public LayerMask groundItems;
-    //private bool isGrounded;
-    private bool doubleJump=true;
 	////Variable to hold double jump
-	//private bool isDoubleJump;
+	private bool isDoubleJump;
 
 	//Vector to hold player position
 	private Vector2 playerVelocity;
+
+    //Animation
+    private Animator playerAnimator;
+    private float movementSpeed = 0;
+    private bool isJumping = false;
 
 	// Use this for initialization
 	void Start () {
@@ -29,95 +31,66 @@ public class PlayerMovement : MonoBehaviour {
         //initilaise player variables
         playerSpeed = 5;
         jumpHeight = 5;
-        //bc2d = transform.GetComponent<BoxCollider2D>();
 
         //get direction the player is moving in
         playerVelocity = GetComponent<Rigidbody2D> ().velocity;
 
+        //initalise animator component 
+        playerAnimator = GetComponent<Animator>();
+
 	}
 
-	//void FixedUpdate (){
+	void FixedUpdate (){
 	//	/*Check whether the player is grounded by getting the position of a empty game object that 
 	//	 * is applied to the player. You then can define the size of the radius that us used from that position
 	//	 * to the other objects. The objects are contained in a layer. This layer can contain a number of objects. We
 	//	 * can use a layer of ground objects.
 	//	 */
-	//	isGrounded = Physics2D.OverlapCircle (groundCheck.position, radiusToGround, groundItems);
-	//}
+		isGrounded = Physics2D.OverlapCircle (groundCheck.position, radiusToGround, groundItems);
+
+        //allow player to jump with an animation
+        isJumping = !isGrounded;
+        playerAnimator.SetBool("isJumping", isJumping);
+
+	}
 	
 	// Update is called once per frame
 	void Update () {
 
-        ////Check if grounded so double jump is reset
-        //if (isGrounded) {
-        //	isDoubleJump = false;
-        //}
-        if (isGrounded())
-        {
-            doubleJump = true;
-        }
+		//Check if grounded so double jump is reset
+		if (isGrounded) {
+			isDoubleJump = false;
+		}
 
 
-        //Get the position of the player at the first step
-        playerVelocity = GetComponent<Rigidbody2D>().velocity;
+		//Get the position of the player at the first step
+		playerVelocity = GetComponent<Rigidbody2D>().velocity;
 
 		//Get the jump axis and have the character jump - using get button instead of axis as we need specific key down actions
-		if (Input.GetButtonDown("Jump")){
-			if(isGrounded())
-            {
-                CharacterJump();
-            }
-            else
-            {
-                if(doubleJump)
-                {
-                    CharacterJump();
-                    doubleJump = false;
-                }
-            }
-
-
+		if (Input.GetButtonDown("Jump") && isGrounded){
+			CharacterJump ();
 
 		}
 
-		////Get the jump axis and have the character jump
-		//if (Input.GetButtonDown("Jump") && !isGrounded && !isDoubleJump){
-		//	CharacterJump ();
-		//	isDoubleJump = true;
-		//}
+		//Get the jump axis and have the character jump
+		if (Input.GetButtonDown("Jump") && !isGrounded && !isDoubleJump){
+			CharacterJump ();
+			isDoubleJump = true;
+		}
 
 		//Use the horizontal axis to have the character move
-	//	if (Input.GetAxis("Horizontal")!=0){
-	//		GetComponent<Rigidbody2D>().velocity = new Vector2(playerSpeed*Input.GetAxis("Horizontal"),playerVelocity.y);
-	//	}
-        if(Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
-        {
-            GetComponent<Rigidbody2D>().velocity = new Vector2(-playerSpeed, GetComponent<Rigidbody2D>().velocity.y);
-        }
-        else
-        {
-           if(Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
-          {
-               GetComponent<Rigidbody2D>().velocity = new Vector2(+playerSpeed, GetComponent<Rigidbody2D>().velocity.y);
-            }
-            else
-            {
-                GetComponent<Rigidbody2D>().velocity = new Vector2(0, GetComponent<Rigidbody2D>().velocity.y);
-            }
-       }
+		if (Input.GetAxis("Horizontal")!=0){
+			GetComponent<Rigidbody2D>().velocity = new Vector2(playerSpeed*Input.GetAxis("Horizontal"),playerVelocity.y);
+
+            //set movement speed that will be used for the transitions of the player states in the animations
+            movementSpeed = Mathf.Abs(Input.GetAxis("Horizontal"));
+		}
+
+        //update the animation settings
+        playerAnimator.SetFloat("speed", movementSpeed);
 	}
 
 	private void CharacterJump(){
-        GetComponent<Rigidbody2D>().velocity = Vector2.up * jumpHeight;
-       
+		GetComponent<Rigidbody2D>().velocity = new Vector2(playerVelocity.x,jumpHeight);
 	}
-
-   private bool isGrounded()
-   {
-        RaycastHit2D raycast2d =Physics2D.BoxCast(GetComponent<BoxCollider2D>().bounds.center, GetComponent<BoxCollider2D>().bounds.size, 0f, Vector2.down,.1f,platformsLayerMask); // hamndles ground detection for jumping
-        Debug.Log(raycast2d.collider);
-        return raycast2d.collider != null;
-
-   }
-    
 }
