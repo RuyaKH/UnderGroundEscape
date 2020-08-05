d05f448ab0552ba38f85639e9820c0bc5a4f3f07
Ruya Kumru-Holroyd <ruya.kh@outlook.com>
2020-08-05 19:29:35 +0100

    changes

---
+++ Assets/Scripts/SoundHit.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoundHit : MonoBehaviour
+{
+    [SerializeField]
+    new AudioSource soundClip;
+    [SerializeField]
+    private AudioClip impact;
+
+    void Start() { 
+        soundClip = (AudioSource)gameObject.AddComponent<AudioSource>();
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("Collision is happening");
+        soundClip.PlayOneShot(impact, 0.7f);
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("Trigger is happening");
+        soundClip.PlayOneShot(impact, 0.7f);
+    }
+}
+
