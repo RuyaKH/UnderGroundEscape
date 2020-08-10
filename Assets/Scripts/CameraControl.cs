d7f1c22433989b1e436b9b9f91278dce04d420bd
Jetsam1 <Akash9105@hotmail.com>
2020-08-10 16:35:38 +0100

    Camera + falling platforms

--- Assets/Scripts/CameraControl.cs
+++ Assets/Scripts/CameraControl.cs
@@ -4,20 +4,45 @@ using UnityEngine;
 
 public class CameraControl : MonoBehaviour
 {
-    public GameObject player;
-    private Vector3 offset;
 
+    public GameObject player;
+    [SerializeField]
+    private Vector3 offset;
+    [SerializeField]
+    private float ypos=-5;
+
+    private float PlayerY;
+   
+    
 
     // Start is called before the first frame update
     void Start()
     {
         offset = transform.position - player.transform.position; //calculates an offset value
+        
+    }
+    void Update()
+    {
+        PlayerY = player.transform.position.y;
 
     }
-
-    // Update is called once per frame
     void LateUpdate()
     {
-        transform.position = player.transform.position + offset;
+        if (PlayerY > ypos)
+        {
+            ypos = PlayerY;
+            transform.position = new Vector3(player.transform.position.x+offset.x, ypos+offset.y, player.transform.position.z+offset.z);//to which we can add offset values later
+        }
+        else
+        {
+            transform.position = new Vector3(player.transform.position.x+offset.x, ypos+offset.y, player.transform.position.z+offset.z);
+        }
     }
+    public void reset()
+    {
+        
+        transform.position = new Vector3(player.transform.position.x+offset.x, player.transform.position.y+offset.y, player.transform.position.z+offset.z);
+    }
+
+
 }
---
+++ Assets/Scripts/PlatformFall.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformFall : MonoBehaviour
+{
+
+    Rigidbody2D rb2d;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called when colliding
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.name.Equals("Jake"))
+        {
+            Invoke("DropPlatform", 1f);
+            Destroy(gameObject, 2f);
+        }
+    }
+    void DropPlatform()
+    {
+        rb2d.isKinematic = false;
+    }
+}
