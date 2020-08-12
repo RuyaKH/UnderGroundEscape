84df709025bd5282754c56a87ccb3d2c829423de
Jetsam1 <Akash9105@hotmail.com>
2020-08-12 15:28:34 +0100

    should open pause menu from in game
    nothing was calling the pause menu to open from within the sample scene

---
+++ Assets/Scripts/OpenPauseMenu.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class OpenPauseMenu : MonoBehaviour
+{
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            SceneManager.LoadScene("Pause");
+        }
+    }
+}
+
